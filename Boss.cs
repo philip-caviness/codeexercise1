e8d1ac8ef4a53515e6d7fe7c9ee90f10acdfce32
pcaviness <philip.caviness@digi.com>
2021-02-01 22:05:13 -0500

    really bad code commit

---
+++ Boss.cs
@@ -0,0 +1,108 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleBadApplication
+{
+    [DataContract]
+    public class Boss : Employee
+    {
+        private const int MONTHS_OF_YEAR = 12;
+        private double perEmployeeSalaryBonus;
+        [DataMember]
+        private Department department;
+        [DataMember]
+        private HashSet<Employee> employees;
+
+
+        /// <summary>
+        /// Constructor for Boss class.
+        /// </summary>
+        /// <param name="department">Department under boss control.</param>
+        public Boss(Department department)
+        {
+            if (department == null) throw new ArgumentNullException("Department is null");
+            this.department = department;
+            employees = new HashSet<Employee>();
+        }
+
+        public Boss(int id, string firstName, string lastName, string job, DateTime birthDate, double monthlySalaryCZK, Department department) :
+            base(id, firstName, lastName, job, birthDate, monthlySalaryCZK)
+        {
+            this.department = department;
+            employees = new HashSet<Employee>();
+        }
+
+        public double SalaryBonusPerEmployee
+        {
+            get => perEmployeeSalaryBonus;
+            set
+            {
+                if (value >= 0.0)
+                    perEmployeeSalaryBonus = value;
+            }
+        }
+
+        public void Add(Employee employee)
+        {
+            if (employee == null) throw new ArgumentNullException("Employee is null");
+            employees.Add(employee);
+        }
+
+        /// <summary>
+        /// Method on remove employee from boss control.
+        /// </summary>
+        /// <param name="employee">Employee which is remove from boss control.</param>
+        public void Remove(Employee employee)
+        {
+            if (employee == null) throw new ArgumentNullException("Employee is null");
+            employees.Remove(employee);
+        }
+
+
+        /// <summary>
+        /// Method which return if employess is under boss control.
+        /// </summary>
+        /// <param name="employee"></param>
+        /// <returns>Return true if employee is found. Else return false.  </returns>
+        public bool HasEmployee(Employee employee)
+        {
+            // todo: complete this function
+        }
+
+
+        /// <summary>
+        /// Method which return all employees.
+        /// </summary>
+        /// <returns>Return all employees in HashSet.</returns>
+        public ISet<Employee> GetEmployees() => new HashSet<Employee>(employees);
+
+        /// <summary>
+        /// Property for get count of employees.
+        /// </summary>
+        /// <returns>Return count of employees.</returns>
+        public int EmployeeCount => employees.Count;
+
+        /// <summary>
+        /// Method which calculate year salary and subemployee bonus (include boss salary).
+        /// </summary>
+        /// <returns>Return value of year department salary.</returns>
+        public override double CalcYearlySalaryCZK() => base.CalcYearlySalaryCZK() + CalculateYearlyBonusForEmployees();
+
+        private double CalculateYearlyBonusForEmployees() => EmployeeCount * perEmployeeSalaryBonus * MONTHS_OF_YEAR;
+
+        /// <summary>
+        /// Method calculate yearly income of all employees (with VAT).
+        /// </summary>
+        /// <returns>Return calculate yearly income after tax.</returns>
+        public override double CalcYearlyIncome() => ApplyTaxRateToSalary(CalcYearlySalaryCZK());
+
+        protected override double ApplyTaxRateToSalary(double salary) => salary * (1 - Boss.TaxRate);
+
+        public override string ToString() => base.ToString() + $";DEPARTMENT - Name: {department.Name} ;CODE: {department.Code}";
+
+    }
+}
---
+++ Employee.cs
@@ -0,0 +1,126 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleBadApplication
+{
+    [DataContract]
+    public class Employee
+    {
+        private DateTime _birthDate;
+
+        [DataMember]
+        public DateTime BirthDate
+        {
+            get
+            {
+                return _birthDate;
+            }
+            set
+            {
+                _birthDate = value;
+                Age = CalculateAge();
+            }
+        }
+
+        private int id;
+
+        [DataMember]
+        public int Id
+        {
+            get => id; set
+            {
+                this.id = value;
+                Employee.nextID = value >= Employee.nextID ? this.id + 1 : Employee.nextID;
+            }
+        }
+
+        [DataMember]
+        public string FirstName { get; set; }
+
+        [DataMember]
+        public string LastName { get; set; }
+
+        [DataMember]
+        public string JobTitle { get; set; }
+
+        [DataMember]
+        public double MonthlySalaryCZK { get; set; }
+
+        public static double TaxRate => 0.21;
+
+        public int Age { get; private set; }
+
+        private static int nextID = 0;
+
+        public Employee(int id, string firstName, string lastName, string jobTitle, DateTime birthDate, double monthlySalaryCZK)
+        {
+            Id = id;
+            FirstName = firstName;
+            LastName = lastName;
+            JobTitle = jobTitle;
+            BirthDate = birthDate;
+            MonthlySalaryCZK = monthlySalaryCZK;
+        }
+
+        public Employee() { }
+
+
+        /// <summary>
+        /// Always contains the next available ID
+        /// </summary>
+        public static int NextID
+        {
+            get
+            {
+                return Employee.nextID++;
+            }
+        }
+
+        /// <summary>
+        /// Method to calculate age of employee in days
+        /// </summary>
+        /// <returns>Age of employee in days as int</returns>
+        private int CalculateAge()
+        {
+            // todo: complete this function
+        }
+
+        /// <summary>
+        /// Method to count sum of 12 salaries (one per month) of the employee
+        /// based on attribute monthlySalaryCZK
+        /// </summary>
+        /// <returns>Sum of all the 12 salaries</returns>
+        public virtual double CalcYearlySalaryCZK()
+        {
+            return MonthlySalaryCZK * 12;
+        }
+
+        /// <summary>
+        /// Method to count the net income of the employee
+        /// based on attribute taxRate
+        /// calls method CalcYearlySalaryCZK
+        /// </summary>
+        /// <returns>Net income of the employee after taxes</returns>
+        public virtual double CalcYearlyIncome()
+        {
+            // todo: complete this function
+        }
+
+        /// <summary>
+        /// Method to calculate salary after taxation
+        /// </summary>
+        /// <param name="salary">Salary of employee</param>
+        /// <returns>Salary after to taxation</returns>
+        protected virtual double ApplyTaxRateToSalary(double salary)
+        {
+            return salary * (1 - TaxRate);
+        }
+
+        public override string ToString() => $"ID:  {Id}; NAME: {FirstName} {LastName}; JOB:{JobTitle}; SALARY: {MonthlySalaryCZK}";
+
+    }
+}
